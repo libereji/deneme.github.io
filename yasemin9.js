bda8ab830df54e2de769255b4c9d6d4444b1e9f8
libereji <82287549+libereji@users.noreply.github.com>
2021-04-11 05:40:51 +0300

    Create yasemin9.js

---
+++ yasemin9.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+                      var defaultnoimage=&quot;http://1.bp.blogspot.com/_u4gySN2ZgqE/SosvnavWq0I/AAAAAAAAArk/yL95WlyTqr0/s400/noimage.png&quot;;
+                      var maxresults=3; // Number Of Posts
+                      removeRelatedDuplicates_thumbs();
+                      printRelatedLabels_thumbs(&quot;<data:post.url/>&quot;);
+                    </script>

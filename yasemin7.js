68d703c91c8f97fa45805b4b7ae74b9a483a06c0
libereji <82287549+libereji@users.noreply.github.com>
2021-04-11 05:37:51 +0300

    Create yasemin7.js

---
+++ yasemin7.js
@@ -0,0 +1,12 @@
+<script type='text/javascript'>
+  function insertAfter(tbh,tgt) {
+  var prt = tgt.parentNode;
+  if (prt.lastChild == tgt) {prt.appendChild(tbh);}
+  else {prt.insertBefore(tbh,tgt.nextSibling);}}
+  var tgt = document.getElementById(&quot;tgtPost&quot;);
+  var midAd1 = document.getElementById(&quot;middle-inside-post-ads&quot;);
+  var showAd1 = tgt.getElementsByTagName(&quot;br&quot;);
+  //<![CDATA[
+$(document).ready(function() {$('a[name="ad-center-post-slot1"]').before($("#paragraph-ad1 .widget-move").html()), $("#paragraph-ad1 .widget-move").html("")})
+ //]]>
+</script>

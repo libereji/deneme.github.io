850ce06292847a9c90f6411afa60fd90d3203ee6
libereji <82287549+libereji@users.noreply.github.com>
2021-04-11 05:36:47 +0300

    Create yasemin6.js

---
+++ yasemin6.js
@@ -0,0 +1,28 @@
+<script type='text/javascript'>
+var mql = window.matchMedia(&#39;screen and (min-width: 350px)&#39;);if (mql.matches){
+var disqus_loaded = false;
+var disqus_shortname = &quot;<data:content/>&quot;;
+var disqus_url = disqus_blogger_current_url;
+var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+if (!disqus_blogger_current_url.length) {
+disqus_blogger_current_url = &quot;<data:blog.url/>&quot;; }
+var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+function disqus() {
+    if (!disqus_loaded)  {
+        disqus_loaded = true;
+        var e = document.createElement(&quot;script&quot;);
+        e.type = &quot;text/javascript&quot;;
+        e.async = true;
+        e.src = &quot;//&quot; + disqus_shortname + &quot;.disqus.com/embed.js&quot;;
+        (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0])
+        .appendChild(e);
+        document.getElementById(&quot;show-comments&quot;).style.display = &quot;none&quot;;document.getElementById(&quot;comments-block&quot;).style.display = &quot;block&quot;;
+    }
+}
+var hash = window.location.hash.substr(1);
+if (hash.length &gt; 8) {
+    if (hash.substring(0, 8) == &quot;comment-&quot;) {
+        disqus();
+    }}}
+</script>

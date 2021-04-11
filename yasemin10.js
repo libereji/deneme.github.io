95d86ed60a7c3d923c77e1aac8251888021638f2
libereji <82287549+libereji@users.noreply.github.com>
2021-04-11 05:41:33 +0300

    Create yasemin10.js

---
+++ yasemin10.js
@@ -0,0 +1,7 @@
+<script defer='defer' expr:src='data:post.commentSrc' type='text/javascript'/><b:template-script inline='true' name='threaded_comments'/>
+	<script type='text/javascript'>
+		blogger.widgets.blog.initThreadedComments(
+		        <data:post.commentJso/>,
+		        <data:post.commentMsgs/>,
+		        <data:post.commentConfig/>);
+	</script>
